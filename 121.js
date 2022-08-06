229bce5142f3a3aa0b6c9b85f9ceefbd174973d3
zzy <104006706+zzystc@users.noreply.github.com>
2022-08-06 18:47:57 +0800

    Update 121.js

--- 121.js
+++ 121.js
@@ -2,14 +2,14 @@
  * @param {number[]} prices
  * @return {number}
  */
-var maxProfit = function(prices) {
+ var maxProfit = function(prices) {
     let n =prices.length;
     let dp = Array.from(new Array(n), () => new Array(2));
     dp[0][0] = 0;
     dp[0][1] = -prices[0];
     for (let i = 1; i < n; i++) {
         dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] + prices[i]);
-        dp[i][1] = Math.max(dp[i - 1][1], -prices[i]);
+        dp[i][1] = Math.max(dp[i - 1][1], 0 - prices[i]);
     }
     return dp[n - 1][0];
 };
