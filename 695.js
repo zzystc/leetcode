492e92b5b5cfe1fc35b15df77a37122561096917
zzy <104006706+zzystc@users.noreply.github.com>
2022-08-11 16:45:28 +0800

    Update 695.js

--- 695.js
+++ 695.js
@@ -25,3 +25,32 @@ var maxAreaOfIsland = function(grid) {
     return res;
 };
 
+/**
+ * @param {number[][]} grid
+ * @return {number}
+ */
+ var maxAreaOfIsland = function(grid) {
+    let ans = 0,
+        row = grid.length,
+        col = grid[0].length,
+        dx = [-1, 1, 0, 0],
+        dy = [0, 0, -1, 1];
+    for (let i = 0; i < row; i++) {
+        for (let j = 0; j < col; j++) {
+            if (grid[i][j] === 0) continue;
+            let queue = [[i, j]],
+                curr = 0;
+            while (queue.length > 0) {
+                let [x, y] = queue.shift();
+                if (x < 0 || x >= row || y < 0 || y >= col || grid[x][y] === 0) continue;
+                curr++;
+                grid[x][y] = 0;
+                for (let k = 0; k < 4; k++) {
+                    queue.push([x + dx[k], y + dy[k]]);
+                }
+            }
+            ans = Math.max(ans, curr);
+        }
+    }
+    return ans;
+};
