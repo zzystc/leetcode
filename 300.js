23a01953ad2a7d84820a4c6e27806ee7cd14090c
zzy <104006706+zzystc@users.noreply.github.com>
2022-08-11 10:53:21 +0800

    Create 300.js

---
+++ 300.js
@@ -0,0 +1,29 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var lengthOfLIS = function(nums) {
+    let n = nums.length;
+    if (n <= 1) {
+        return n;
+    }
+    let tail = [nums[0]];
+    for (let i = 0; i < n; i++) {
+        if (nums[i] > tail[tail.length - 1]) {
+            tail.push(nums[i]);
+        } else {
+            let left = 0,
+                right = tail.length - 1;
+            while (left < right) {
+                let mid = (left + right) >> 1;
+                if (tail[mid] < nums[i]) {
+                    left = mid + 1;
+                } else {
+                    right = mid;
+                }
+            }
+            tail[left] = nums[i];
+        }
+    }
+    return tail.length;
+};
