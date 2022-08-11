0b21f02c6a1a0d654e98bcfb73a65932c52bc5a4
zzy <104006706+zzystc@users.noreply.github.com>
2022-08-11 21:14:07 +0800

    Update 142.js

--- 142.js
+++ 142.js
@@ -21,3 +21,40 @@ var detectCycle = function(head) {
     }
     return null;
 };
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+ var detectCycle = function(head) {
+    if (head === null) {
+        return null;
+    }
+    let slow = head,
+        fast = head;
+    while (fast !== null) {
+        slow = slow.next;
+        if (fast.next !== null) {
+            fast = fast.next.next;
+        } else {
+            return null;
+        }
+        if (fast === slow) {
+            let fast = head;
+            while (fast !== slow) {
+                fast = fast.next;
+                slow = slow.next;
+            }
+            return fast;
+        }
+    }
+    return null;
+};
