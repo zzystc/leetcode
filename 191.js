0f98a1237ee04faefca5b16fbf4ac4fc49fd87f6
zzy <104006706+zzystc@users.noreply.github.com>
2022-08-12 16:48:55 +0800

    Update 191.js

--- 191.js
+++ 191.js
@@ -11,3 +11,16 @@ var hammingWeight = function(n) {
     }
     return ret;
 };
+
+/**
+ * @param {number} n - a positive integer
+ * @return {number}
+ */
+ var hammingWeight = function(n) {
+    let ret = 0;
+    while (n) {
+        n &= n - 1;
+        ret++;
+    }
+    return ret;
+};
