66b887bf8ca0cf6b4051e849e255ab6d5ea50b25
zzy <104006706+zzystc@users.noreply.github.com>
2022-08-12 17:04:50 +0800

    Update 231.js

--- 231.js
+++ 231.js
@@ -5,3 +5,12 @@
 var isPowerOfTwo = function(n) {
     return n > 0 && (n & (n - 1)) === 0;
 };
+
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+ var isPowerOfTwo = function(n) {
+    let MAX = 1 << 30;
+    return n > 0 && MAX % n === 0;
+};
