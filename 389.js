2d5775f7ce7b167119557676d8d113674f09591c
zzy <104006706+zzystc@users.noreply.github.com>
2022-08-12 20:07:50 +0800

    Update 389.js

--- 389.js
+++ 389.js
@@ -16,3 +16,20 @@ var findTheDifference = function(s, t) {
     }
     return '';
 };
+
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {character}
+ */
+ var findTheDifference = function(s, t) {
+    let as = 0,
+        at = 0;
+    for (let i = 0; i < s.length; i++) {
+        as += s[i].charCodeAt();
+    }
+    for (let i = 0; i < t.length; i++) {
+        at += t[i].charCodeAt();
+    }
+    return String.fromCharCode(at - as);
+};
