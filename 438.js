e01fdc7efd0b79f563c9a5dfb80f6bd780e33e9d
zzy <104006706+zzystc@users.noreply.github.com>
2022-08-12 11:15:31 +0800

    Update 438.js

--- 438.js
+++ 438.js
@@ -59,7 +59,7 @@ var findAnagrams = function(s, p) {
         need[x]++;
     }
     for (let i = 0; i < s.length; i++) {
-        const j = i -pLen;
+        const j = i - pLen;
         if (s[i] in need && ++win[s[i]] === need[s[i]]) val++;
         if (s[j] in need && win[s[j]]-- === need[s[j]]) val--;
         if (val === len) res.push(j + 1)
