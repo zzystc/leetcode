c542ae1b4fde7ef35080b9936e566624c4378101
zzy <104006706+zzystc@users.noreply.github.com>
2022-08-12 22:14:11 +0800

    Update 169.js

--- 169.js
+++ 169.js
@@ -19,3 +19,23 @@ var majorityElement = function(nums) {
         if (obj[num] > half) return num;
     }
 };
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+ var majorityElement = function(nums) {
+    let count = 1,
+        majority = nums[0];
+    for (let i = 1; i < nums.length; i++) {
+        if (count === 0) {
+            majority = nums[i];
+        }
+        if (nums[i] === majority) {
+            count++;
+        } else {
+            count--;
+        }
+    }
+    return majority;
+};
