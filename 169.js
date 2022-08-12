da5fc5f670eae200ad412d8f30b05518509c5b6f
zzy <104006706+zzystc@users.noreply.github.com>
2022-08-12 22:14:05 +0800

    Update 169.js

--- 169.js
+++ 169.js
@@ -6,3 +6,16 @@ var majorityElement = function(nums) {
     nums.sort((a, b) => a - b);
     return nums[nums.length >> 1];
 };
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+ var majorityElement = function(nums) {
+    let half = nums.length / 2;
+    let obj = {};
+    for (let num of nums) {
+        obj[num] = (obj[num] || 0) + 1;
+        if (obj[num] > half) return num;
+    }
+};
