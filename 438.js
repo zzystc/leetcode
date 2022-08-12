9160a867faab6ad4412c0f19ba16f02c02f06543
zzy <104006706+zzystc@users.noreply.github.com>
2022-08-12 11:14:26 +0800

    Update 438.js

--- 438.js
+++ 438.js
@@ -38,3 +38,31 @@ var findAnagrams = function(s, p) {
     }
     return res;
 };
+
+/**
+ * @param {string} s
+ * @param {string} p
+ * @return {number[]}
+ */
+ var findAnagrams = function(s, p) {
+    const res = [], 
+          win = {},
+          need = {},
+          pLen = p.length;
+    let len = 0,
+        val = 0;
+    for (const x of p) {
+        if (need[x] === undefined) {
+            need[x] = win[x] = 0;
+            len++;
+        }
+        need[x]++;
+    }
+    for (let i = 0; i < s.length; i++) {
+        const j = i -pLen;
+        if (s[i] in need && ++win[s[i]] === need[s[i]]) val++;
+        if (s[j] in need && win[s[j]]-- === need[s[j]]) val--;
+        if (val === len) res.push(j + 1)
+    }
+    return res;
+};
