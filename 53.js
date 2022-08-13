8b5cc60bc99544bbf908cbdb426e9e2fe72f66e9
zzy <104006706+zzystc@users.noreply.github.com>
2022-08-13 10:42:08 +0800

    Create 53.js

---
+++ 53.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxSubArray = function(nums) {
+    const dp = [];
+    let res = (dp[0] = nums[0]);
+    for (let i = 1; i < nums.length; i++) {
+        dp[i] = nums[i];
+        if (dp[i - 1] > 0) {
+            dp[i] += dp[i - 1];
+        }
+        res = Math.max(dp[i], res);
+    }
+    return res; 
+};
