5741e0cf9b38394bfcf53921a0ff05b5c9236bcf
zzy <104006706+zzystc@users.noreply.github.com>
2022-08-13 16:56:58 +0800

    Update 22.js

--- 22.js
+++ 22.js
@@ -19,3 +19,29 @@ var generateParenthesis = function(n) {
     generate('', n, n);
     return res;
 };
+
+/**
+ * @param {number} n
+ * @return {string[]}
+ */
+ var generateParenthesis = function(n) {
+    if (n == 0) return [];
+    const res = [],
+          track = [];
+    backtrack(n, n, track, res);
+    return res;
+    function backtrack(left, right, track, res) {
+        if (left < 0 || right < 0) return;
+        if (right < left) return;
+        if (left === 0 && right === 0) {
+            res.push(track.join(''));
+            return;
+        }
+        track.push('(');
+        backtrack(left - 1, right, [...track], res);
+        track.pop();
+        track.push(')');
+        backtrack(left, right - 1, [...track], res);
+        track.pop();
+    }
+};
