bf93d15971a651e0d49aedfd22751f14b415351a
zzy <104006706+zzystc@users.noreply.github.com>
2022-08-13 19:25:53 +0800

    Create 36.js

---
+++ 36.js
@@ -0,0 +1,22 @@
+/**
+ * @param {character[][]} board
+ * @return {boolean}
+ */
+var isValidSudoku = function(board) {
+    let rows = {},
+        columns = {},
+        boxes = {};
+    for (let i = 0; i < 9; i++) {
+        for (let j = 0; j < 9; j++) {
+            let num = board[i][j];
+            if (num != '.') {
+                let boxIndex = parseInt((i / 3)) * 3 + parseInt(j / 3);
+                if (rows[i + '-' + num] || columns[j + '-' + num] || boxes[boxIndex + '-' + num]) return false;
+                rows[i + '-' + num] = true;
+                columns[j + '-' + num] = true;
+                boxes[boxIndex + '-' +num] = true;
+            }
+        }
+    }
+    return true;
+};
