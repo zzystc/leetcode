af3dfd8dafd219fe96b226840565744e7e5ad29c
zzy <104006706+zzystc@users.noreply.github.com>
2022-08-13 14:42:36 +0800

    Update 53.js

--- 53.js
+++ 53.js
@@ -28,3 +28,41 @@ var maxSubArray = function(nums) {
     })
     return maxAns;
 };
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+ function crossSum(nums, left, right, mid) {
+    if (left === right) {
+        return nums[left];
+    }
+    let leftMaxSum = Number.MIN_SAFE_INTEGER,
+        leftSum = 0;
+    for (let i = mid; i >= left; i--) {
+        leftSum += nums[i];
+        leftMaxSum = Math.max(leftMaxSum, leftSum);
+    }
+    let rightMaxSum = Number.MIN_SAFE_INTEGER,
+        rightSum = 0;
+    for (let i = mid + 1; i <= right; i++) {
+        rightSum += nums[i];
+        rightMaxSum = Math.max(rightMaxSum, rightSum);
+    }
+    return leftMaxSum + rightMaxSum;
+}
+
+function _maxSubArray(nums, left, right) {
+    if (left === right) {
+        return nums[left];
+    }
+    const mid = (left + right) >> 1;
+    const lsum = _maxSubArray(nums, left, mid);
+    const rsum = _maxSubArray(nums, mid + 1, right);
+    const cross = crossSum(nums, left, right, mid);
+    return Math.max(lsum, rsum, cross);
+}
+
+var maxSubArray = function(nums) {
+    return _maxSubArray(nums, 0, nums.length - 1);
+};
