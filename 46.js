77c771e9060727bf74213ed926cdd637b06a4d60
zzy <104006706+zzystc@users.noreply.github.com>
2022-08-14 14:31:20 +0800

    Create 46.js

---
+++ 46.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var permute = function(nums) {
+    const res = [],
+          path = [];
+    backtracking(nums, nums.length, []);
+    return res;
+
+    function backtracking(n, k, used) {
+        if (path.length === k) {
+            res.push(Array.from(path));
+            return;
+        }
+        for (let i = 0; i < k; i++) {
+            if (used[i]) continue;
+            path.push(n[i]);
+            used[i] = true;
+            backtracking(n, k, used);
+            path.pop();
+            used[i] = false;
+        }
+    }
+};
