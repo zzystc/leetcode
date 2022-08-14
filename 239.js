9dafd082879ebf42c8e9b19c4bc4b1242186d5a4
zzy <104006706+zzystc@users.noreply.github.com>
2022-08-14 20:36:14 +0800

    Update 239.js

--- 239.js
+++ 239.js
@@ -97,3 +97,26 @@ var maxSlidingWindow = function(nums, k) {
     }
     return ans;
 }
+
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number[]}
+ */
+ var maxSlidingWindow = function(nums, k) {
+    const q = [],
+          ans = [];
+    for (let i = 0; i < nums.length; i++) {
+        while (q.length && nums[i] >= nums[q[q.length - 1]]){
+            q.pop();
+        }
+        q.push(i);
+        while (q[0] <= i - k) {
+            q.shift();
+        }
+        if (i >= k - 1) {
+            ans.push(nums[q[0]]);
+        }
+    }
+    return ans;
+};
