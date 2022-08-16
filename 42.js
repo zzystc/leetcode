5814b81d281e98417c1931d4c621fcf737859538
zzy <104006706+zzystc@users.noreply.github.com>
2022-08-16 22:09:38 +0800

    Update 42.js

--- 42.js
+++ 42.js
@@ -48,3 +48,27 @@ var trap = function(height) {
     }
     return ans;
 };
+
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+ var trap = function(height) {
+    let ans = 0;
+    let left = 0,
+        right = height.length - 1;
+    let leftMax = 0,
+        rightMax = 0;
+    while (left < right) {
+        leftMax  = Math.max(leftMax, height[left]);
+        rightMax = Math.max(rightMax, height[right]);
+        if (height[left] < height[right]) {
+            ans += leftMax - height[left];
+            left++;
+        } else {
+            ans += rightMax - height[right];
+            right--;
+        }
+    }
+    return ans;
+};
