19e86aa8e249dff938b538cd3d110cf94254d030
zzy <104006706+zzystc@users.noreply.github.com>
2022-08-19 17:19:37 +0800

    Update 215.js

--- 215.js
+++ 215.js
@@ -135,3 +135,44 @@ class Heap {
         [a[i], a[j]] = [a[j], a[i]];
     }
 };
+
+
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+ var findKthLargest = function(nums, k) {
+    const n = nums.length;
+    const quick = (l, r) => {
+        if (l > r) return;
+        let random = Math.floor(Math.random() * (r - l + 1)) + l;
+        swap(nums, random, r);
+        
+        let pivotIndex = partition(nums, l, r);
+        if (n - k < pivotIndex) {
+            quick(l, pivotIndex - 1);
+        } else {
+            quick(pivotIndex + 1, r);
+        }
+    }
+    quick(0, n - 1);
+    return nums[n - k];
+};
+
+function partition(nums, left, right) {
+    let pivot = nums[right];
+    let pivotIndex = left;
+    for (let i = left; i < right; i++) {
+        if (nums[i] < pivot) {
+            swap(nums, i, pivotIndex);
+            pivotIndex++;
+        }
+    }
+    swap(nums, right, pivotIndex);
+    return pivotIndex;
+}
+
+function swap(nums, p, q) {
+    [nums[p], nums[q]] = [nums[q], nums[p]];
+}
