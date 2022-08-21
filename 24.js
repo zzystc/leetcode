ee278e78707067dd9dc3a1c7dbdb6dd62f893ccc
zzy <104006706+zzystc@users.noreply.github.com>
2022-08-21 14:41:36 +0800

    Update 24.js

--- 24.js
+++ 24.js
@@ -18,3 +18,30 @@ var swapPairs = function(head) {
     newHead.next = head;
     return newHead;
 };
+
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+ var swapPairs = function(head) {
+    const dummyNode = new ListNode();
+    dummyNode.next = head;
+    let temp = dummyNode;
+    while (temp.next !== null && temp.next.next !== null) {
+        const node1 = temp.next,
+              node2 = temp.next.next;
+        temp.next = node2;
+        node1.next = node2.next;
+        node2.next = node1;
+        temp = node1;
+    }
+    return dummyNode.next;
+};
