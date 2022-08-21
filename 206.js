c7fa15a65df4057a0df0face215b4f35854ee758
zzy <104006706+zzystc@users.noreply.github.com>
2022-08-21 10:19:08 +0800

    Update 206.js

--- 206.js
+++ 206.js
@@ -46,3 +46,25 @@ var reverseList = function(head) {
     }
     return prev;
 };
+
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+ var reverseList = function(head) {
+    if (head === null || head.next === null) {
+        return head;
+    }
+    const newHead = reverseList(head.next);
+    head.next.next = head;
+    head.next = null;
+    return newHead;
+};
