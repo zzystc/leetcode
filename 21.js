4f283e0561bc7516226603258f86ebbab3bfb570
zzy <104006706+zzystc@users.noreply.github.com>
2022-08-21 21:20:41 +0800

    Update 21.js

--- 21.js
+++ 21.js
@@ -23,3 +23,33 @@ var mergeTwoLists = function(list1, list2) {
         return list2;
     }
 };
+
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} list1
+ * @param {ListNode} list2
+ * @return {ListNode}
+ */
+ var mergeTwoLists = function(list1, list2) {
+    const preHead = new ListNode();
+    let prev = preHead;
+    while (list1 !== null && list2 !== null) {
+        if (list1.val < list2.val) {
+            prev.next = list1;
+            list1 = list1.next;
+        } else {
+            prev.next = list2;
+            list2 = list2.next;
+        }
+        prev = prev.next;
+    }
+    prev.next = list1 === null ? list2 : list1;
+    return preHead.next;
+};
