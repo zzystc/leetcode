3d6e7d96bb9a93f7d3233ffdd8a822cfde2ced9d
zzy <104006706+zzystc@users.noreply.github.com>
2022-08-22 21:33:57 +0800

    Update 23.js

--- 23.js
+++ 23.js
@@ -40,3 +40,43 @@ var mergeKLists = function(lists) {
     }
     return mergeLists(lists, 0, lists.length);
 };
+
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode[]} lists
+ * @return {ListNode}
+ */
+ var mergeKLists = function(lists) {
+    if (lists.length <= 1) {
+        return lists[0] || null;
+    }
+    const newLists = [];
+    for (let i = 0; i < lists.length; i += 2) {
+        newLists.push(merge(lists[i], lists[i + 1] || null));
+    }
+    return mergeKLists(newLists);
+};
+
+const merge = (list1, list2) => {
+    const dummy = new ListNode();
+    let cur = dummy;
+    while (list1 && list2) {
+        if (list1.val < list2.val) {
+            cur.next = list1;
+            list1 = list1.next;
+        } else {
+            cur.next = list2;
+            list2 = list2.next;
+        }
+        cur = cur.next;
+    }
+    cur.next = list1 === null ? list2 : list1;
+    return dummy.next;
+}
