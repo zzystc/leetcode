ae2db09e51aed9996bbf0e9d8db6d5144a6efaa9
zzy <104006706+zzystc@users.noreply.github.com>
2022-08-22 22:49:47 +0800

    Update 703.js

--- 703.js
+++ 703.js
@@ -115,3 +115,116 @@ KthLargest.prototype.add = function(val) {
         return this.data.length;
     }
 }
+
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode[]} lists
+ * @return {ListNode}
+ */
+ var mergeKLists = function(lists) {
+    const res = new ListNode();
+    let p = res;
+    const h = new Heap(comparator = (a, b) => a.val - b.val);
+    lists.forEach(
+        l => {if (l) {
+            h.offer(l);
+        }
+    })
+    while (h.size()) {
+        const n = h.poll();
+        p.next = n;
+        p = p.next;
+        if (n.next) {
+            h.offer(n.next);
+        }
+    }
+    return res.next;
+};
+
+class Heap {
+    constructor(comparator = (a, b) => a - b, data = []) {
+        this.data = data;
+        this.comparator = comparator;
+        this.heapify();
+    }
+    heapify() {
+        if (this.size() < 2) {
+            return;
+        }
+        for (let i = this.size() >> 1; i >= 0; i++) {
+            this.bubbleDown(i);
+        }
+    }
+    peek() {
+        if (this.size() === 0) {
+            return null;
+        }
+        return this.data[0];
+    }
+    offer(value) {
+        this.data.push(value);
+        this.bubbleUp(this.size() - 1);
+    }
+    poll() {
+        if (this.size() === 0) {
+            return null;
+        }
+        const res = this.data[0];
+        const last = this.data.pop();
+        if (this.size() !== 0) {
+            this.data[0] = last;
+            this.bubbleDown(0);
+        }
+        return res;
+    }
+    bubbleUp(index) {
+        while (index > 0) {
+            const parentIndex = (index - 1) >> 1;
+            if (this.comparator(this.data[index], this.data[parentIndex]) < 0) {
+                this.swap(index, parentIndex);
+                index = parentIndex;
+            } else {
+                break;
+            }
+        }
+    }
+    bubbleDown(index) {
+        const lastIndex = this.size() - 1;
+        while (true) {
+            const leftIndex = index * 2 + 1;
+            const rightIndex = index * 2 + 2;
+            let findIndex = index;
+            if (
+                leftIndex <= lastIndex &&
+                this.comparator(this.data[leftIndex], this.data[findIndex]) < 0
+            ) {
+                findIndex = leftIndex;
+            }
+            if (
+                rightIndex <= lastIndex &&
+                this.comparator(this.data[rightIndex], this.data[findIndex]) < 0
+            ) {
+                findIndex = rightIndex;
+            }
+            if (index !== findIndex) {
+                this.swap(index, findIndex);
+                index = findIndex;
+            } else {
+                break;
+            }
+        }
+    }
+    swap(index1, index2) {
+        [this.data[index1], this.data[index2]] = [this.data[index2], this.data[index1]];
+    }
+    size() {
+        return this.data.length;
+    }
+}
