343743249d9ddfbca34d734962f7d3efe4779bf8
zzy <104006706+zzystc@users.noreply.github.com>
2022-08-23 23:35:30 +0800

    Update 905.js

--- 905.js
+++ 905.js
@@ -5,3 +5,23 @@
 var sortArrayByParity = function(nums) {
     return nums.sort((a, b) => (a & 1) - (b & 1));
 };
+
+
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+ var sortArrayByParity = function(nums) {
+    let l = 0,
+        r = nums.length - 1;
+    while (l !== r) {
+        while (r > 0 && nums[r] & 1) {
+            r--;
+        }
+        while (l < r && (nums[l] & 1) === 0) {
+            l++;
+        }
+        [nums[l], nums[r]] = [nums[r], nums[l]];
+    }
+    return nums;
+};
