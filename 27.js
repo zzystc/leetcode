ba391e21ef099009c2b5870c0bf0d0b4cdfb1c40
zzy <104006706+zzystc@users.noreply.github.com>
2022-08-23 21:55:05 +0800

    Update 27.js

--- 27.js
+++ 27.js
@@ -14,3 +14,23 @@ var removeElement = function(nums, val) {
     }
     return left;
 };
+
+
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+ var removeElement = function(nums, val) {
+    let left = 0,
+        right = nums.length;
+    while (left < right) {
+        if (nums[left] === val) {
+            nums[left] = nums[right - 1];
+            right--;
+        } else {
+            left++;
+        }
+    }
+    return left;
+};
