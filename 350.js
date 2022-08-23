0edfcd5c0f6e71ac72981e8a9dd69a60c86213ef
zzy <104006706+zzystc@users.noreply.github.com>
2022-08-23 21:26:12 +0800

    Update 350.js

--- 350.js
+++ 350.js
@@ -25,3 +25,28 @@ var intersect = function(nums1, nums2) {
     }
     return res;
 };
+
+
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+ var intersect = function(nums1, nums2) {
+    nums1.sort((a, b) => a - b);
+    nums2.sort((a, b) => a - b);
+    const res = [];
+    let p1 = p2 = 0;
+    while (p1 < nums1.length && p2 < nums2.length) {
+        if (nums1[p1] > nums2[p2]) {
+            p2++;
+        } else if (nums1[p1] < nums2[p2]) {
+            p1++;
+        } else {
+            res.push(nums2[p2]);
+            p1++;
+            p2++
+        }
+    }
+    return res;
+};
