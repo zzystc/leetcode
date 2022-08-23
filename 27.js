9d736bac730466024462a857273625d128717129
zzy <104006706+zzystc@users.noreply.github.com>
2022-08-23 21:41:44 +0800

    Create 27.js

---
+++ 27.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+var removeElement = function(nums, val) {
+    const n = nums.length;
+    let left = 0;
+    for (let right = 0; right < n; right++) {
+        if (nums[right] !== val) {
+            nums[left] = nums[right];
+            left++
+        }
+    }
+    return left;
+};
