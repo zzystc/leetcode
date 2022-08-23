0438798bdd25d5f06251e604eac4ecad51975b74
zzy <104006706+zzystc@users.noreply.github.com>
2022-08-23 19:37:13 +0800

    Update 167.js

--- 167.js
+++ 167.js
@@ -23,3 +23,22 @@ var twoSum = function(numbers, target) {
     }
     return [-1, -1];
 };
+
+
+/**
+ * @param {number[]} numbers
+ * @param {number} target
+ * @return {number[]}
+ */
+ var twoSum = function(numbers, target) {
+    let [left, right] = [0, numbers.length - 1];
+    while (left < right) {
+        if (numbers[left] + numbers[right] > target) {
+            right--;
+        } else if (numbers[left] + numbers[right] < target) {
+            left++;
+        } else {
+            return [left + 1, right + 1];
+        }
+    }
+};
