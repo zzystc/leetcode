c0d5dac46afcdc2371e3af8121e24867e5619b94
zzy <104006706+zzystc@users.noreply.github.com>
2022-08-23 11:20:45 +0800

    Create 933.js

---
+++ 933.js
@@ -0,0 +1,22 @@
+var RecentCounter = function() {
+    this.queue = [];
+};
+
+/** 
+ * @param {number} t
+ * @return {number}
+ */
+RecentCounter.prototype.ping = function(t) {
+    this.queue.push(t);
+    const time = t - 3000;
+    while (this.queue[0] < time) {
+        this.queue.shift();
+    }
+    return this.queue.length;
+};
+
+/**
+ * Your RecentCounter object will be instantiated and called as such:
+ * var obj = new RecentCounter()
+ * var param_1 = obj.ping(t)
+ */
