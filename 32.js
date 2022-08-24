d0d8c1502c3445f5843a07f8e8d5a01eb28d7993
zzy <104006706+zzystc@users.noreply.github.com>
2022-08-24 15:16:21 +0800

    Update 32.js

--- 32.js
+++ 32.js
@@ -26,3 +26,27 @@ var longestValidParentheses = function(s) {
     }
     return maxLen;
 };
+
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+ var longestValidParentheses = function(s) {
+    let maxLen = 0;
+    let stack = [];
+    stack.push(-1);
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] === '(') {
+            stack.push(i);
+        } else {
+            stack.pop();
+            if (stack.length) {
+                maxLen = Math.max(maxLen, i - stack[stack.length - 1]);
+            } else {
+                stack.push(i);
+            }
+        }
+    }
+    return maxLen;
+};
