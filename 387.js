dc76a0b6abfd10e0d1fd95cf7a617ba068dd58f5
zzy <104006706+zzystc@users.noreply.github.com>
2022-08-24 18:41:13 +0800

    Update 387.js

--- 387.js
+++ 387.js
@@ -14,3 +14,25 @@ var firstUniqChar = function(s) {
     }
     return -1;
 };
+
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+ var firstUniqChar = function(s) {
+    const position = new Map();
+    const q = [];
+    for (let [i, ch] of [...s].entries()) {
+        if (!position.has(ch)) {
+            position.set(ch, i);
+            q.push([ch, i]);
+        } else {
+            position.set(ch, -1);
+            while (q.length && position.get(q[0][0]) === -1) {
+                q.shift();
+            }
+        }
+    }
+    return q.length ? q[0][1] : -1;
+};
