4f644bc56c88aa7adb8c03be96e8f13ad060eb3d
zzy <104006706+zzystc@users.noreply.github.com>
2022-08-24 15:33:13 +0800

    Update 32.js

--- 32.js
+++ 32.js
@@ -50,3 +50,40 @@ var longestValidParentheses = function(s) {
     }
     return maxLen;
 };
+
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+ var longestValidParentheses = function(s) {
+    let maxLen = 0,
+        left = 0,
+        right = 0;
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] === '(') {
+            left++
+        } else {
+            right++;
+        }
+        if (left === right) {
+            maxLen = Math.max(maxLen, left + right);
+        } else if (left < right) {
+            left = right = 0;
+        }
+    }
+    left = right = 0;
+    for (let i = s.length - 1; i >= 0; i--) {
+        if (s[i] === '(') {
+            left++;
+        } else {
+            right++;
+        }
+        if (left === right) {
+            maxLen = Math.max(maxLen, left + right);
+        } else if (left > right) {
+            left = right = 0;
+        }
+    }
+    return maxLen;
+};
