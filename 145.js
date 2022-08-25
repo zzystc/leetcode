1dfad653444a534214e6b4af331c4b3eb11e2fca
zzy <104006706+zzystc@users.noreply.github.com>
2022-08-25 22:14:58 +0800

    Create 145.js

---
+++ 145.js
@@ -0,0 +1,19 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var postorderTraversal = function(root, res =[]) {
+    if (!root) return res;
+    postorderTraversal(root.left, res);
+    postorderTraversal(root.right, res);
+    res.push(root.val);
+    return res;
+};
