52a97d6a7b6e971e8f60dbe288c28d09ae56212c
zzy <104006706+zzystc@users.noreply.github.com>
2022-08-25 10:22:42 +0800

    Update 151.js

--- 151.js
+++ 151.js
@@ -5,3 +5,33 @@
 var reverseWords = function(s) {
     return s.trim().replace(/\s+/g, ' ').split(' ').reverse().join(' ');
 };
+
+
+/**
+ * @param {string} s
+ * @return {string}
+ */
+ var reverseWords = function(s) {
+    let left = 0,
+        right = s.length - 1,
+        queue = [],
+        word = '';
+    while (s.charAt(left) === ' ') {
+        left++;
+    }
+    while (s.charAt(right) === ' ') {
+        right--;
+    }
+    while (left <= right) {
+        let char  = s.charAt(left);
+        if (char === ' ' && word) {
+            queue.unshift(word);
+            word = '';
+        } else if (char !== ' ') {
+            word += char;
+        }
+        left++;
+    }
+    queue.unshift(word);
+    return queue.join(' ');
+};
