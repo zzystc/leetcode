868a8fcc4156917c7062b63d0aa09f8bccca458e
zzy <104006706+zzystc@users.noreply.github.com>
2022-08-25 22:20:22 +0800

    Update 145.js

--- 145.js
+++ 145.js
@@ -17,3 +17,33 @@ var postorderTraversal = function(root, res =[]) {
     res.push(root.val);
     return res;
 };
+
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+ var postorderTraversal = function(root, res = []) {
+    const stack = [];
+    if (root) stack.push(root);
+    while (stack.length) {
+        const node = stack.pop();
+        if (!node) {
+            res.push(stack.pop().val);
+            continue;
+        }
+        stack.push(node);
+        stack.push(null);
+        if (node.right) stack.push(node.right);
+        if (node.left) stack.push(node.left);
+    }
+    return res;
+};
