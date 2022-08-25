bec7e99e2e529e1534a9aa68695d2757173344c1
zzy <104006706+zzystc@users.noreply.github.com>
2022-08-25 21:58:59 +0800

    Update 144.js

--- 144.js
+++ 144.js
@@ -17,3 +17,33 @@ var preorderTraversal = function(root, res = []) {
     preorderTraversal(root.right, res)
     return res;
 };
+
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+ var preorderTraversal = function(root, res = []) {
+    const stack = [];
+    if (root) stack.push(root);
+    while (stack.length) {
+        const node = stack.pop();
+        if (!node) {
+            res.push(stack.pop().val);
+            continue;
+        }
+        if (node.right) stack.push(node.right);
+        if (node.left) stack.push(node.left);
+        stack.push(node);
+        stack.push(null);
+    }
+    return res;
+};
