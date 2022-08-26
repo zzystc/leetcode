87293214e904fec3116b7aa56af2acad6a9456f0
zzy <104006706+zzystc@users.noreply.github.com>
2022-08-26 15:09:23 +0800

    Update 102.js

--- 102.js
+++ 102.js
@@ -61,3 +61,41 @@ function dfs(root, step, res) {
         dfs(root.right, step + 1, res);
     }
 }
+
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[][]}
+ */
+ var levelOrderBottom = function(root) {
+    if (root === null) {
+        return [];
+    }
+    const queue = [root];
+    const res = [];
+
+    while (queue.length !== 0) {
+        const subRes = [];
+        const levelSize = queue.length;
+        for (let i = 0; i < levelSize; i++) {
+            const cur = queue.shift();
+            subRes.push(cur.val);
+            if (cur.left) {
+                queue.push(cur.left);
+            }
+            if (cur.right) {
+                queue.push(cur.right);
+            }
+        }
+        res.unshift(subRes);
+    }
+    return res;
+};
