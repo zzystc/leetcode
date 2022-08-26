c380e336557b215caa85125146b7a788eb7ca6c0
zzy <104006706+zzystc@users.noreply.github.com>
2022-08-26 21:56:58 +0800

    Create 127.js

---
+++ 127.js
@@ -0,0 +1,27 @@
+/**
+ * @param {string} beginWord
+ * @param {string} endWord
+ * @param {string[]} wordList
+ * @return {number}
+ */
+var ladderLength = function(beginWord, endWord, wordList) {
+    const wordSet = new Set(wordList);
+    const queue = [];
+    queue.push([beginWord, 1]);
+    while (queue.length !== 0) {
+        const [word, level] = queue.shift();
+        if (word === endWord) {
+            return level;
+        }
+        for (let i = 0; i < word.length; i++) {
+            for (let c = 97; c < 123; c++) {
+                const newWord = word.slice(0, i) + String.fromCharCode(c) + word.slice(i + 1);
+                if (wordSet.has(newWord)) {
+                    queue.push([newWord, level + 1]);
+                    wordSet.delete(newWord);
+                }
+            }
+        }
+    }
+    return 0;
+};
