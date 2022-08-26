6af8fd1b814b91ac7d8bd9335257d04e813b67a1
zzy <104006706+zzystc@users.noreply.github.com>
2022-08-26 11:17:10 +0800

    Update 98.js

--- 98.js
+++ 98.js
@@ -23,3 +23,34 @@ const helper = (root, lower, upper) => {
     }
     return helper(root.left, lower, root.val) && helper(root.right, root.val, upper);
 }
+
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+ var isValidBST = function(root) {
+    let stack = [],
+        inorder = -Infinity;
+    while (stack.length || root !== null) {
+        while (root !== null) {
+            stack.push(root);
+            root = root.left;
+        }
+        root = stack.pop();
+        if (root.val <= inorder) {
+            return false;
+        }
+        inorder = root.val;
+        root = root.right;
+    }
+    return true;
+};
