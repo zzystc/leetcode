a5efa0ec3dd3918c31c06f608e80f4142af53db5
zzy <104006706+zzystc@users.noreply.github.com>
2022-08-26 19:54:22 +0800

    Update 111.js

--- 111.js
+++ 111.js
@@ -26,3 +26,34 @@ var minDepth = function(root) {
     }
     return ans + 1;
 };
+
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+ var minDepth = function(root) {
+    if (root === null) return 0;
+    let q = [root];
+    let depth = 1;
+    while (q.length !== 0) {
+        let len = q.length;
+        for (let i = 0; i < len; i++) {
+            let cur = q.shift();
+            if (cur.left === null && cur.right === null) {
+                return depth;
+            }
+            if (cur.left) q.push(cur.left);
+            if (cur.right) q.push(cur.right);
+        }
+        depth++;
+    }
+};
