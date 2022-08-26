22946ad585724c1efbdd1fef14976a5d495b4821
zzy <104006706+zzystc@users.noreply.github.com>
2022-08-26 15:35:42 +0800

    Update 104.js

--- 104.js
+++ 104.js
@@ -19,3 +19,40 @@ var maxDepth = function(root) {
         return Math.max(left, right) + 1;
     }
 };
+
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+ var maxDepth = function(root) {
+    if(root === null) {
+        return 0;
+    }
+    const queue = [root];
+    let deepth = 1;
+    while (queue.length !== 0) {
+        const levelSize = queue.length;
+        for (let i = 0; i < levelSize; i++) {
+            const cur = queue.shift();
+            if (cur.left) {
+                queue.push(cur.left);
+            }
+            if (cur.right) {
+                queue.push(cur.right);
+            }
+            }
+        if (queue.length !== 0) {
+            deepth++;
+        }
+    }
+    return deepth;
+};
