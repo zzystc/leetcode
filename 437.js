b4e98b870a9018ad539fa5173489ecf79e283034
zzy <104006706+zzystc@users.noreply.github.com>
2022-08-27 17:41:52 +0800

    Update 437.js

--- 437.js
+++ 437.js
@@ -27,3 +27,35 @@ const rootSum = (root, targetSum) => {
     ret += rootSum(root.right, targetSum - root.val);
     return ret;
 }
+
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} targetSum
+ * @return {number}
+ */
+ var pathSum = function(root, targetSum) {
+    const prefix = new Map();
+    prefix.set(0, 1);
+    return dfs(root, prefix, 0, targetSum);
+};
+
+const dfs = (root, prefix, curr, targetSum) => {
+    if (root === null) {return 0}
+    let ret = 0;
+    curr += root.val;
+    ret = prefix.get(curr - targetSum) || 0;
+    prefix.set(curr, (prefix.get(curr) || 0) + 1);
+    ret += dfs(root.left, prefix, curr, targetSum);
+    ret += dfs(root.right, prefix, curr, targetSum);
+    prefix.set(curr, (prefix.get(curr) || 0) - 1);
+    return ret;
+}
