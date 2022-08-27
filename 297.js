78e892e0cde15ab116349fb96734a6c3acd41bc4
zzy <104006706+zzystc@users.noreply.github.com>
2022-08-27 13:29:37 +0800

    Update 297.js

--- 297.js
+++ 297.js
@@ -46,3 +46,71 @@ var deserialize = function(data) {
  * Your functions will be called as such:
  * deserialize(serialize(root));
  */
+
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+
+/**
+ * Encodes a tree to a single string.
+ *
+ * @param {TreeNode} root
+ * @return {string}
+ */
+ var serialize = function(root) {
+    const queue = [root];
+    let res = [];
+    while (queue.length !== 0) {
+        const node = queue.shift();
+        if (node) {
+            res.push(node.val);
+            queue.push(node.left);
+            queue.push(node.right);
+        } else {
+            res.push('X');
+        }
+    }
+    return res.join(',');
+};
+
+/**
+ * Decodes your encoded data to tree.
+ *
+ * @param {string} data
+ * @return {TreeNode}
+ */
+var deserialize = function(data) {
+    if (data === 'X') return null;
+    const list = data.split(',');
+    const root = new TreeNode(list[0]);
+    const queue = [root];
+    let cursor = 1;
+    
+    while (cursor < list.length) {
+        const node = queue.shift();
+        const leftVal = list[cursor];
+        const rightVal = list[cursor + 1];
+        if (leftVal !== 'X') {
+            const leftNode = new TreeNode(leftVal);
+            node.left = leftNode;
+            queue.push(leftNode);
+        }
+        if (rightVal !== 'X') {
+            const rightNode = new TreeNode(rightVal);
+            node.right = rightNode;
+            queue.push(rightNode);
+        }
+        cursor += 2;
+    }
+    return root;
+};
+
+/**
+ * Your functions will be called as such:
+ * deserialize(serialize(root));
+ */
