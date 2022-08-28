30cc4d3a7126c06bfcda7b1f2e1f7f33a9c44333
zzy <104006706+zzystc@users.noreply.github.com>
2022-08-29 00:28:44 +0800

    Update 199.js

--- 199.js
+++ 199.js
@@ -25,3 +25,33 @@ var rightSideView = function(root) {
     dfs(root, 0, arr);
     return arr;
 };
+
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+ var rightSideView = function(root) {
+    if (!root) {return []};
+    let queue =[root];
+    let arr = [];
+    while (queue.length !== 0) {
+        let len = queue.length;
+        while (len !== 0) {
+            let node = queue.shift();
+            if (len === 1) {arr.push(node.val)};
+            if (node.left !== null) {queue.push(node.left)};
+            if (node.right !== null) {queue.push(node.right)};
+            len--;
+        }
+    }
+    return arr;
+};
