2bf1db7f65bf5d89f44b7655ced73e6c320bf2ee
zzy <104006706+zzystc@users.noreply.github.com>
2022-08-31 15:21:23 +0800

    Update 547.js

--- 547.js
+++ 547.js
@@ -51,3 +51,64 @@ const dfs = (isConnected, visited, rows, i) => {
     }
     return count;
 };
+
+
+/**
+ * @param {number[][]} isConnected
+ * @return {number}
+ */
+ var findCircleNum = function(isConnected) {
+    const rows = isConnected.length;
+    const uf = new UnionFind(rows);
+
+    for (let i = 0; i < rows; i++) {
+        for (let j = i + 1; j < rows; j++) {
+            if (isConnected[i][j] === 1) {
+                uf.union(i, j);
+            }
+        }
+    }
+    return uf.getCount();
+};
+
+class UnionFind {
+    constructor(n) {
+        this.count = n;
+        this.parent = new Array(n);
+        this.size = new Array(n);
+        for (let i = 0; i < n; i++) {
+            this.parent[i] = i;
+            this.size[i] = 1;
+        }
+    }
+
+    union(p, q) {
+        let rootP = this.find(p);
+        let rootQ = this.find(q);
+        if (rootP === rootQ) return;
+        if (this.size[rootP] > this.size[rootQ]) {
+            this.parent[rootQ] = rootP;
+            this.size[rootP] += this.size[rootQ];
+        } else {
+            this.parent[rootP] = rootQ;
+            this.size[rootQ] += this.size[rootP];
+        }
+        this.count--;
+    }    
+
+    isConnected(p, q) {
+        return this.find(p) === this.find(q); 
+    }
+
+    find(x) {
+        while (this.parent[x] !== x) {
+            this.parent[x] = this.parent[this.parent[x]];
+            x = this.parent[x];
+        }
+        return x;
+    } 
+
+    getCount() {
+        return this.count;
+    }
+}
