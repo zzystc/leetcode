5eb0f14d59c6005040cedc4cd2071e305d94b32a
zzy <104006706+zzystc@users.noreply.github.com>
2022-08-31 10:18:15 +0800

    Update 200.js

--- 200.js
+++ 200.js
@@ -25,3 +25,43 @@ function turnZero(i, j, grid) {
     turnZero(i + 1, j, grid);
     turnZero(i - 1, j, grid);
 }
+
+
+/**
+ * @param {character[][]} grid
+ * @return {number}
+ */
+ var numIslands = function(grid) {
+    let count = 0;
+    let queue = [];
+    for (let i = 0; i < grid.length; i++) {
+        for (let j = 0; j < grid[0].length; j++) {
+            if (grid[i][j] === '1') {
+                count++;
+                grid[i][j] = '0';
+                queue.push([i, j]);
+                turnZero(queue, grid);
+            }
+        }
+    }
+    return count;
+};
+
+function turnZero(queue, grid) {
+    const dirs = [[0, 1],
+                  [0, -1],
+                  [1, 0],
+                  [-1, 0]];
+    while (queue.length !== 0) {
+        const cur = queue.shift();
+        for (const dir of dirs) {
+            const x = cur[0] + dir[0];
+            const y = cur[1] + dir[1];
+            if (x < 0 || x >= grid.length ||
+                y < 0 || y >= grid[0].length ||
+                grid[x][y] === '0') continue;
+            grid[x][y] = '0';
+            queue.push([x, y]);
+        }
+    }
+} 
