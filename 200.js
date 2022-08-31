b045ada77d0260be5e358c88c46fe8aa9272f3ce
zzy <104006706+zzystc@users.noreply.github.com>
2022-08-31 11:05:19 +0800

    Update 200.js

--- 200.js
+++ 200.js
@@ -65,3 +65,77 @@ function turnZero(queue, grid) {
         }
     }
 } 
+
+
+/**
+ * @param {character[][]} grid
+ * @return {number}
+ */
+ var numIslands = function(grid) {
+    let m = grid.length;
+    if (m === 0) {return 0}
+    let n = grid[0].length;
+    const dummy = -1;
+    const dirs = [[1, 0],
+                  [0, 1]];
+    const uf = new UnionFind(m * n);
+    for (let x = 0; x < m; x++) {
+        for (let y = 0; y < n; y++) {
+            if (grid[x][y] === '0') {
+                uf.union(n * x + y, dummy);
+            } else if (grid[x][y] === '1') {
+                for (let d of dirs) {
+                    let r = x + d[0];
+                    let c = y + d[1];
+                    if (r >= m || c >= n) continue;
+                    if (grid[r][c] === '1') {
+                        uf.union(n * x + y, n * r + c);
+                    }
+                }
+            }
+        }
+    }
+    return uf.getCount();
+};
+
+class UnionFind {
+    constructor(n) {
+        this.count = n;
+        this.parent = new Array(n);
+        this.size = new Array(n);
+        for (let i = 0; i < n; i++) {
+            this.parent[i] = i;
+            this.size[i] = 1;
+        }
+    }
+
+    union(p, q) {
+        let rootP = this.find(p);
+        let rootQ = this.find(q);
+        if (rootP === rootQ) return;
+        if (this.size[rootP] > this.size[rootQ]) {
+            this.parent[rootQ] = rootP;
+            this.size[rootP] += this.size[rootQ];
+        } else {
+            this.parent[rootP] = rootQ;
+            this.size[rootQ] += this.size[rootP];
+        }
+        this.count--;
+    }
+
+    isConnected(p, q) {
+        return this.find(p) === this.find(q);
+    }
+
+    find(x) {
+        while (this.parent[x] !== x) {
+            this.parent[x] = this.parent[this.parent[x]];
+            x = this.parent[x];
+        }
+        return x;
+    }
+
+    getCount() {
+        return this.count;
+    }
+}
