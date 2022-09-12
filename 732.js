3b03d4dfd11ae44f89e359bbd5d3b99754613c6c
zzy <104006706+zzystc@users.noreply.github.com>
2022-09-12 20:40:14 +0800

    Update 732.js

--- 732.js
+++ 732.js
@@ -33,3 +33,31 @@ MyCalendarThree.prototype.update = function(start, end, l, r, idx) {
  * var obj = new MyCalendarThree()
  * var param_1 = obj.book(start,end)
  */
+
+
+ var MyCalendarThree = function() {
+    this.planMap = {};
+};
+
+/** 
+ * @param {number} start 
+ * @param {number} end
+ * @return {number}
+ */
+MyCalendarThree.prototype.book = function(start, end) {
+    this.planMap[start] = (this.planMap[start] || 0) + 1;
+    this.planMap[end] = (this.planMap[end] || 0) - 1;
+    let res = 0,
+        count = 0;
+    for (let item of Object.values(this.planMap)) {
+        count += item;
+        res = Math.max(res, count);
+    }
+    return res;
+};
+
+/**
+ * Your MyCalendarThree object will be instantiated and called as such:
+ * var obj = new MyCalendarThree()
+ * var param_1 = obj.book(start,end)
+ */
