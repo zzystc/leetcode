7ebe12d1940215847903c3f20a0eee9650c6b1a4
zzy <104006706+zzystc@users.noreply.github.com>
2022-09-13 18:43:26 +0800

    Create 899.js

---
+++ 899.js
@@ -0,0 +1,17 @@
+/**
+ * @param {string} s
+ * @param {number} k
+ * @return {string}
+ */
+var orderlyQueue = function(s, k) {
+    if (k === 1) {
+        let ans = s;
+        for (let i = 0; i < s.length - 1; i++) {
+            const n = s.length;
+            s = s.substring(1, n) + s[0];
+            ans = ans < s ? ans : s;
+        }
+        return ans;
+    }
+    return [...s].sort().join('');
+};
