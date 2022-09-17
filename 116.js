3f6ef91d8ed35c6ae7ad5ed1d70eda02927c5a6f
zzy <104006706+zzystc@users.noreply.github.com>
2022-09-17 19:10:17 +0800

    Update 116.js

--- 116.js
+++ 116.js
@@ -30,3 +30,35 @@ var connect = function(root) {
     }
     return root;
 };
+
+
+/**
+ * // Definition for a Node.
+ * function Node(val, left, right, next) {
+ *    this.val = val === undefined ? null : val;
+ *    this.left = left === undefined ? null : left;
+ *    this.right = right === undefined ? null : right;
+ *    this.next = next === undefined ? null : next;
+ * };
+ */
+
+/**
+ * @param {Node} root
+ * @return {Node}
+ */
+ var connect = function(root) {
+    if (root === null) {return root};
+    let leftMost = root;
+    while (leftMost.left !== null) {
+        let head = leftMost;
+        while (head !== null){
+            head.left.next = head.right;
+            if (head.next !== null) {
+                head.right.next = head.next.left;
+            }
+            head = head.next;
+        }
+        leftMost = leftMost.left;
+    }
+    return root;
+};
